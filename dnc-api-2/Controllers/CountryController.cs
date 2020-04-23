6184bda6b3c6079821e54373fd57fcb41c46086d
Alistair Robson <adrobson@hotmail.com>
2020-04-23 13:23:39 +0100

    add cors to controllers

--- dnc-api-2/Controllers/CountryController.cs
+++ dnc-api-2/Controllers/CountryController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace dnc_api_2.Controllers
 {
     [ApiController]
+    [EnableCors("OpenCORSPolicy")]
     [Route("[controller]")]
     public class CountryController : ControllerBase
     {
